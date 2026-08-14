527adf0d1c01fb7dfc4fb85e100519b7e7224647
Robin Rodriguez <robinrz@amazon.com>
2026-08-14 13:59:42 +0000

    fix(accountaccess): validate application ARN against ID

--- internal/service/accountaccess/application_test.go
+++ internal/service/accountaccess/application_test.go
@@ -39,7 +39,7 @@ func testAccAccountAccessApplication_basic(t *testing.T) {
 				Config: testAccApplicationConfig_basic(rName),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					testAccCheckApplicationExists(ctx, t, resourceName, &v),
-					acctest.CheckResourceAttrRegionalARNFormat(ctx, resourceName, names.AttrARN, "account-access", "application/{id}"),
+					resource.TestCheckResourceAttrPair(resourceName, names.AttrARN, resourceName, "id"),
 					resource.TestCheckResourceAttrSet(resourceName, "identity_center_application_arn"),
 					resource.TestCheckResourceAttr(resourceName, names.AttrStatus, "ACTIVE"),
 				),
