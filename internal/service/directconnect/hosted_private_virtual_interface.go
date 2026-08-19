ac636d488014345ddc7283fede8d55b479b88379
Robin Rodriguez <robinrz@amazon.com>
2026-08-19 01:02:44 +0000

    resource/aws_dx_hosted_private_virtual_interface: extend update timeout

--- internal/service/directconnect/hosted_private_virtual_interface.go
+++ internal/service/directconnect/hosted_private_virtual_interface.go
@@ -118,7 +118,7 @@ func resourceHostedPrivateVirtualInterface() *schema.Resource {
 
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(10 * time.Minute),
-			Update: schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(30 * time.Minute),
 			Delete: schema.DefaultTimeout(10 * time.Minute),
 		},
 	}
